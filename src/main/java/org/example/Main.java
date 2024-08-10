f6114f01d402e35ae3754a070151ebe02c207ea3
Momoko Ota <shinmako1139@icloud.com>
2024-08-10 15:55:17 +0900

    0で割った時のエラーの修正

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,6 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -11,52 +10,61 @@ public class Main {
     //問題１
 
     Scanner scanner = new Scanner(System.in);
-    //1番目の数字を入力してください:
-    System.out.println("1番目の数字を入力してください：");
-    int firstNumber = scanner.nextInt();
 
-    //演算子を入力してください (+, -, *, /): +
-    System.out.println("演算子を入力してください (+, -, *, /):");
-    char operator = scanner.next().charAt(0);
+    try {
+      //1番目の数字を入力してください:
+      System.out.println("1番目の数字を入力してください：");
+      int firstNumber = scanner.nextInt();
 
-    //2番目の数字を入力してください:
-    System.out.println("2番目の数字を入力してください：");
-    int secondNumber = scanner.nextInt();
+      //演算子を入力してください (+, -, *, /): +
+      System.out.println("演算子を入力してください (+, -, *, /):");
+      char operator = scanner.next().charAt(0);
 
-    int result;
-    int remainder;
+      //2番目の数字を入力してください:
+      System.out.println("2番目の数字を入力してください：");
+      int secondNumber = scanner.nextInt();
 
-    switch (operator) {
-      case '+' -> {
-        result = firstNumber + secondNumber;
-        System.out.println(
-            "計算結果：" + firstNumber + operator + secondNumber + "=" + result);
+      int result;
+      int remainder;
+
+      switch (operator) {
+        case '+' -> {
+          result = firstNumber + secondNumber;
+          System.out.println(
+              "計算結果：" + firstNumber + operator + secondNumber + "=" + result);
+        }
+
+        case '-' -> {
+          result = firstNumber - secondNumber;
+          System.out.println(
+              "計算結果：" + firstNumber + operator + secondNumber + "=" + result);
+        }
+
+        case '*' -> {
+          result = firstNumber * secondNumber;
+          System.out.println(
+              "計算結果：" + firstNumber + operator + secondNumber + "=" + result);
+        }
+
+        case '/' -> {
+          if (secondNumber == 0) {
+            System.out.println("0で割ることはできません");
+          } else {
+            result = firstNumber / secondNumber;
+            remainder = firstNumber % secondNumber;
+            System.out.println(
+                "計算結果：" + firstNumber + operator + secondNumber + "=" + result + "あまり"
+                    + remainder);
+          }
+        }
+
+        default -> System.out.println("入力内容に誤りがあります。");
       }
 
-      case '-' -> {
-        result = firstNumber - secondNumber;
-        System.out.println(
-            "計算結果：" + firstNumber + operator + secondNumber + "=" + result);
-      }
-
-      case '*' -> {
-        result = firstNumber * secondNumber;
-        System.out.println(
-            "計算結果：" + firstNumber + operator + secondNumber + "=" + result);
-      }
-
-      case '/' -> {
-        result = firstNumber / secondNumber;
-        remainder = firstNumber % secondNumber;
-        System.out.println(
-            "計算結果：" + firstNumber + operator + secondNumber + "=" + result + "あまり"
-                + remainder);
-      }
-
-      default -> System.out.println("入力内容に誤りがあります");
+    }catch (InputMismatchException e){
+      System.out.println("無効な入力です。数値を入力してください。");
+    }finally {
+      scanner.close();
     }
-
-    scanner.close();
-
   }
 }
